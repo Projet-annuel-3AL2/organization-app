17e059c9d8717bdcc66c819e514c118d68298f47
pooetitu <pooetitu@gmail.com>
2021-05-13 17:52:58 +0200

    Add admin handling

--- src/main/java/com/agirpourtous/gui/controllers/popups/ProjectUsersManagementPopup.java
+++ src/main/java/com/agirpourtous/gui/controllers/popups/ProjectUsersManagementPopup.java
@@ -1,6 +1,7 @@
 package com.agirpourtous.gui.controllers.popups;
 
 import com.agirpourtous.gui.controllers.Controller;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
@@ -10,6 +11,9 @@ public class ProjectUsersManagementPopup extends Popup {
     public Label utilisateur;
     public ComboBox usersNonMemberList;
     public ComboBox usersMemberList;
+    public ChoiceBox usersAdminList;
+    public ChoiceBox usersNotAdminList;
+    public ComboBox usersNotMemberList;
 
     ProjectUsersManagementPopup(Controller controller) throws IOException {
         super("Management des utilisateurs", "project_users_management_popup", controller);
