37f1efc4f87f57158f19d98871c5f163a6ca966b
pooetitu <pooetitu@gmail.com>
2021-05-16 03:09:35 +0200

    Reformat code

--- src/main/java/com/agirpourtous/gui/controllers/popups/CreateTicketPopup.java
+++ src/main/java/com/agirpourtous/gui/controllers/popups/CreateTicketPopup.java
@@ -1,13 +1,13 @@
 package com.agirpourtous.gui.controllers.popups;
 
-        import com.agirpourtous.core.api.requests.AddTicketRequest;
-        import com.agirpourtous.gui.controllers.Controller;
-        import com.agirpourtous.gui.controllers.ProjectDetailsController;
-        import javafx.application.Platform;
-        import javafx.scene.control.TextArea;
-        import javafx.scene.control.TextField;
+import com.agirpourtous.core.api.requests.AddTicketRequest;
+import com.agirpourtous.gui.controllers.Controller;
+import com.agirpourtous.gui.controllers.ProjectDetailsController;
+import javafx.application.Platform;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-        import java.io.IOException;
+import java.io.IOException;
 
 public class CreateTicketPopup extends Popup {
     public TextField nameField;
