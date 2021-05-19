01653e63fd7bb36ca838f4200d11b96a569edfa5
pooetitu <pooetitu@gmail.com>
2021-05-19 12:32:56 +0200

    Apply fix sending null fields

--- src/main/java/com/agirpourtous/core/api/requests/AddTicketRequest.java
+++ src/main/java/com/agirpourtous/core/api/requests/AddTicketRequest.java
@@ -1,15 +1,42 @@
 package com.agirpourtous.core.api.requests;
 
+import com.agirpourtous.core.models.TicketStatus;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 @JsonAutoDetect
 public class AddTicketRequest {
+
+    private String assigneeId;
     private String title;
     private String description;
+    private TicketStatus status;
+    private float estimatedDuration;
+    private int priority;
 
-    public AddTicketRequest(String title, String description) {
+    public AddTicketRequest(String assigneeId, String title, String description, float estimatedDuration, int priority, TicketStatus status) {
+        this.assigneeId = assigneeId;
         this.title = title;
         this.description = description;
+        this.estimatedDuration = estimatedDuration;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public AddTicketRequest(String text, String text1) {
+        this.title = text;
+        this.description = text1;
+    }
+
+    public AddTicketRequest(TicketStatus status) {
+        this.status = status;
+    }
+
+    public String getAssigneeId() {
+        return assigneeId;
+    }
+
+    public void setAssigneeId(String assigneeId) {
+        this.assigneeId = assigneeId;
     }
 
     public String getTitle() {
@@ -26,5 +53,29 @@ public class AddTicketRequest {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public float getEstimatedDuration() {
+        return estimatedDuration;
+    }
+
+    public void setEstimatedDuration(float estimatedDuration) {
+        this.estimatedDuration = estimatedDuration;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public TicketStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TicketStatus status) {
+        this.status = status;
     }
 }
