9e5aa90fb4fbbb38ed9d39854f60f496df3383e0
pooetitu <pooetitu@gmail.com>
2021-06-12 12:57:56 +0200

    Implement cli password recovery

--- src/main/java/com/agirpourtous/cli/menus/AuthMenu.java
+++ src/main/java/com/agirpourtous/cli/menus/AuthMenu.java
@@ -1,6 +1,8 @@
 package com.agirpourtous.cli.menus;
 
 import com.agirpourtous.cli.menus.forms.AuthForm;
+import com.agirpourtous.cli.menus.forms.ForgotPasswordForm;
+import com.agirpourtous.cli.menus.forms.ResetPasswordForm;
 import com.agirpourtous.core.api.APIClient;
 
 public class AuthMenu extends Menu {
@@ -15,6 +17,25 @@ public class AuthMenu extends Menu {
                         .onErrorContinue((res, r) -> res.printStackTrace())
                         .doOnSuccess(res -> new MainMenu(client))
                         .doOnError(res -> new AuthMenu())
+                        .block();
+            }
+        });
+        addAction(new Action("Mot de passe oublié ou nouveau compte") {
+            @Override
+            public void execute() {
+                client.getAuthService()
+                        .forgotPassword(new ForgotPasswordForm().askEntries().getUsername())
+                        .doOnSuccess(res -> System.out.println("Un mail contenant le code de récupération vous a été envoyé"))
+                        .doOnTerminate(AuthMenu::new)
+                        .block();
+            }
+        });
+        addAction(new Action("Réinitialiser le mot de passe avec un code de récupération") {
+            @Override
+            public void execute() {
+                client.getAuthService()
+                        .resetPassword(new ResetPasswordForm().askEntries())
+                        .doOnTerminate(AuthMenu::new)
                         .block();
             }
         });
